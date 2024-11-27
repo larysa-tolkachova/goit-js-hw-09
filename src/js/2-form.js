7048160782528e40382e90e8869844067680ab5c
Larysa <seagull2610@gmail.com>
2024-11-27 13:44:59 +0100

    add task 1

---
+++ src/js/2-form.js
@@ -0,0 +1,22 @@
+const formData = {
+    email: "",
+    message: ""
+}
+
+const LS_key = "feedback-form-state";
+
+const form = document.querySelector("feedback-form");
+const input = form.querySelector("input");
+const textarea = form.querySelector("textarea");
+
+form.addEventListener("submit")
+
+
+
+
+ 
+
+
+
+
+
