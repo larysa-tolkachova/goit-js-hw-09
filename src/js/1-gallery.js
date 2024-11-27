16d38e6be44eb089360e31da09eeaa3155682001
Larysa <seagull2610@gmail.com>
2024-11-27 06:51:49 +0100

    add start

---
+++ src/js/1-gallery.js
@@ -0,0 +1,6 @@
+
+import SimpleLightbox from "simplelightbox";// Описаний в документації
+
+import "simplelightbox/dist/simple-lightbox.min.css";// Додатковий імпорт стилів
+
+const lightbox = new SimpleLightbox('.gallery a', { /* options */ });
